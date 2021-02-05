2af0aef1ddf8ad202a33325bd547e8126b35d0b1
Mathilde <dutrieuxmathilde@gmail.com>
2021-02-05 08:38:41 +0100

    Modifications

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,6 @@
+import '../css/app.scss';
+import Background from './background';
+
 class App {
     constructor () {
         this.initApp();
@@ -5,6 +8,7 @@ class App {
 
     initApp () {
       // Start application
+      new Background(); // Si on passe des paramètres ici, ils seront récupérés dans les parenthèses de la méthode constructeur
     }
 }
 
