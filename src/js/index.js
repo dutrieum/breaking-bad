050f15b2d03e2186a9ff2d661a48e0e2f1d7c2a6
Mathilde <dutrieuxmathilde@gmail.com>
2021-03-18 16:49:13 +0100

    Begin transition fonction

--- src/js/index.js
+++ src/js/index.js
@@ -26,9 +26,30 @@ class App {
         })
     }
 
+    Transition() {
+      var first_link = document.getElementById("perso");
+      var second_link = document.getElementById("cit");
+      var first_section = document.getElementById("homestyle");
+      var second_section = document.getElementById("charstyle");
+      var back_home = document.getElementById("logo");
+      back_home.addEventListener('click', () => {
+        first_section.style.setProperty('display', 'block');
+        second_section.style.setProperty('display', 'none');
+      });
+      first_link.addEventListener('click', () => {
+        first_section.style.setProperty('display', 'none');
+        second_section.style.setProperty('display', 'block');
+      });
+      second_link.addEventListener('click', () => {
+        second_section.style.setProperty('display', 'none');
+        first_section.style.setProperty('display', 'block');
+      });
+    }
+
     initApp () {
       // Start application
       this.GetApi();
+      this.Transition();
       new Background(); // Si on passe des paramètres ici, ils seront récupérés dans les parenthèses de la méthode constructeur
       new Greeting();
       new Quote();
